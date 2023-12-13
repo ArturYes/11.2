1c5a617aca95d3b6ad43b188642c2f80785e4a26
ArturYes <153022144+ArturYes@users.noreply.github.com>
2023-12-13 19:45:23 +0600

    Update str_func.py
    добавление второй функции заглавление букв

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def uppercase_string(text):
     return text.upper()
+
+def uppercase_string(text):
+    """
+    Функция принимает строку и возвращает ее с заглавными буквами.
+    
+    Args:
+        text (str): Входная строка.
+        
+    Returns:
+        str: Строка с заглавными буквами.
+    """
+    return text.upper()
+    
