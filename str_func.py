b920e16f5c543fbbc81ad062c5417000e4ce486b
Artur Esenov <artur.yessenov@gmail.com>
2023-12-13 20:00:26 +0600

    добавление функции принимает строку и возвращает

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def uppercase_string(text):
     return text.upper()
+
+
+def capitalize_words(text):
+    """
+    Функция принимает строку и возвращает ее с заглавными первыми буквами каждого слова.
+
+    Args:
+        text (str): Входная строка.
+
+    Returns:
+        str: Строка с заглавными первыми буквами каждого слова.
+    """
+    return ' '.join(word.capitalize() for word in text.split())
